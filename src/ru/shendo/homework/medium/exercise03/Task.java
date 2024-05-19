f041c4dbc4904b813e5db3f06808f8ddbd9e95e6
Aleksei Shendo <shendo.alex@gmail.com>
2024-05-19 13:29:41 +0300

    Task medium 03 - done

---
+++ src/ru/shendo/homework/medium/exercise03/Task.java
@@ -0,0 +1,38 @@
+package ru.shendo.homework.medium.exercise03;
+
+import java.util.Scanner;
+
+/*
+Расчет факториала:
+Пользователь вводит число, программа должна вычислить его факториал.
+Входные данные 5
+Результат 120
+ */
+public class Task {
+    public static void main(String[] args) {
+        calculateFactorial();
+    }
+
+    public static void calculateFactorial() {
+        final String INPUT_DIALOG = "Введите число: ";
+        final String FACTORIAL_WARNING = "Факториал от числа меньше 1 - не вычисляется.";
+        int numberForCalculation;
+        int count = 1;
+        int result = 1;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(INPUT_DIALOG);
+        numberForCalculation = scanner.nextInt();
+        scanner.close();
+
+        if (numberForCalculation < 1) {
+            System.out.println(FACTORIAL_WARNING);
+        } else {
+            while (count <= numberForCalculation) {
+                result *= count;
+                count++;
+            }
+            System.out.println(result);
+        }
+    }
+}
