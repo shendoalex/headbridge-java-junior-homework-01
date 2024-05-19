22eb16d7e64b5191e2c2c5805a92cee4cde3ee5a
Aleksei Shendo <shendo.alex@gmail.com>
2024-05-19 13:02:46 +0300

    Task medium 02 - done

---
+++ src/ru/shendo/homework/medium/exercise02/Task.java
@@ -0,0 +1,35 @@
+package ru.shendo.homework.medium.exercise02;
+
+import java.util.Scanner;
+
+/*
+Определение високосного года:
+Напишите программу, которая определяет, является ли заданный год високосным.
+Входные данные 2024
+Результат Год високосный.
+ */
+
+public class Task {
+    public static void main(String[] args) {
+        checkYearForLeap();
+
+    }
+
+    public static void checkYearForLeap() {
+        final String INPUT_DIALOG = "Введите год: ";
+        final String YEAR_IS_LEAP = "Год високосный.";
+        final String YEAR_IS_NOT_LEAP = "Год не високосный.";
+        int year;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(INPUT_DIALOG);
+        year = scanner.nextInt();
+        scanner.close();
+
+        if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
+            System.out.println(YEAR_IS_LEAP);
+        } else {
+            System.out.println(YEAR_IS_NOT_LEAP);
+        }
+    }
+}
